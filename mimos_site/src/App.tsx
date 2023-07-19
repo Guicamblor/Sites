e9f62abaa98b3f510f031ec96e394937f4df8e7e
GCamblor <guicamblor@gmail.com>
2023-07-19 10:51:07 -0300

    Primeiro Commit
    Pastas iniciais e configurações iniciais

---
+++ mimos_site/src/App.tsx
@@ -0,0 +1,46 @@
+import styles from './style';
+import { 
+  Main, 
+  NavBar, 
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer 
+} from './components'
+
+
+const App = () => (
+  <div className="text-white bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar/>
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Main/>
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+
+  </div>
+)
+
+export default App
---
+++ mimos_site/src/components/index.ts
@@ -0,0 +1,24 @@
+//Importação dos componentes
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Main from "./Main";
+import NavBar from "./NavBar";
+
+export {
+    NavBar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Main
+}
