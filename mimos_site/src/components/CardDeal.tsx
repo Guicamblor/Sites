97c0188a5e7c8461fc58d295f719eecc7597c9b4
GCamblor <guicamblor@gmail.com>
2023-07-24 17:50:57 -0300

    Site base pré pronto
    Site base para fazer as coisas pré pronto, falta 2 partes

--- mimos_site/src/components/CardDeal.tsx
+++ mimos_site/src/components/CardDeal.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Outra cardizinho de sessão <br className="sm:block hidden" /> parte de baixo</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>descrição do novo cardizinho</p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+)
 
 export default CardDeal
