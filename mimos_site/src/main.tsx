c5a74ae09e8b414704fabbe6270f6d889a26c572
Augusto Rodrigo Camblor Santos <augustor.camblor@gmail.com>
2023-07-25 17:54:21 -0300

    Criação do Menu, implementação e importação da biblioteca MUI

--- mimos_site/src/main.tsx
+++ mimos_site/src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './page/home.tsx'
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   id: "1",
+  //   element: <App />,
+  // },
+  {
+    path: "",
+    id: "2",
+    element: <Home />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
