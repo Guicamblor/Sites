988e825437254f2bc102315d9eb9d295c7616721
Augusto Rodrigo Camblor Santos <augustor.camblor@gmail.com>
2023-07-25 18:35:37 -0300

    Criação do Tema, alteração da cor do ícone de usuário e da barra do menu

---
+++ mimos_site/src/styles/menuStyles.tsx
@@ -0,0 +1,11 @@
+import { createTheme} from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+
+
+export const menuTheme = createTheme({
+    palette:{
+      primary: {
+        main: green[400] // #66bb6a
+      }
+    }
+})
