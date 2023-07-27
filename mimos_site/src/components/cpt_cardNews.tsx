f48d1e3b5efe995ec68773515f013b2c51f1e92a
Augusto Rodrigo Camblor Santos <augustor.camblor@gmail.com>
2023-07-27 15:31:59 -0300

    Criado o Painel de noticias, não ajustado css

--- mimos_site/src/components/cpt_cardNews.tsx
+++ mimos_site/src/components/cpt_cardNews.tsx
@@ -1,126 +1,162 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+// import { useTheme } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import MobileStepper from '@mui/material/MobileStepper';
+// import Paper from '@mui/material/Paper';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+// import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+// import SwipeableViews from 'react-swipeable-views';
+// import { autoPlay } from 'react-swipeable-views-utils';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+// const images = [
+//   {
+//     label: 'San Francisco – Oakland Bay Bridge, United States',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+//   },
+//   {
+//     label: 'Bird',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+//   },
+//   {
+//     label: 'Bali, Indonesia',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+//   },
+//   {
+//     label: 'Goč, Serbia',
+//     imgPath:
+//       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+//   },
+// ];
 
-const images = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-];
+// export const cardNews = () => {
+//   const theme = useTheme();
+//   const [activeStep, setActiveStep] = React.useState(0);
+//   const maxSteps = images.length;
+
+//   const handleNext = () => {
+//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+//   };
+
+//   const handleBack = () => {
+//     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+//   };
+
+//   const handleStepChange = (step: number) => {
+//     setActiveStep(step);
+//   };
+
+//   return (
+//     <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+//       <Paper
+//         square
+//         elevation={0}
+//         sx={{
+//           display: 'flex',
+//           alignItems: 'center',
+//           height: 50,
+//           pl: 2,
+//           bgcolor: 'background.default',
+//         }}
+//       >
+//         <Typography>{images[activeStep].label}</Typography>
+//       </Paper>
+//       <AutoPlaySwipeableViews
+//         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+//         index={activeStep}
+//         onChangeIndex={handleStepChange}
+//         enableMouseEvents
+//       >
+//         {images.map((step, index) => (
+//           <div key={step.label}>
+//             {Math.abs(activeStep - index) <= 2 ? (
+//               <Box
+//                 component="img"
+//                 sx={{
+//                   height: 255,
+//                   display: 'block',
+//                   maxWidth: 400,
+//                   overflow: 'hidden',
+//                   width: '100%',
+//                 }}
+//                 src={step.imgPath}
+//                 alt={step.label}
+//               />
+//             ) : null}
+//           </div>
+//         ))}
+//       </AutoPlaySwipeableViews>
+//       <MobileStepper
+//         steps={maxSteps}
+//         position="static"
+//         activeStep={activeStep}
+//         nextButton={
+//           <Button
+//             size="small"
+//             onClick={handleNext}
+//             disabled={activeStep === maxSteps - 1}
+//           >
+//             Next
+//             {theme.direction === 'rtl' ? (
+//               <KeyboardArrowLeft />
+//             ) : (
+//               <KeyboardArrowRight />
+//             )}
+//           </Button>
+//         }
+//         backButton={
+//           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+//             {theme.direction === 'rtl' ? (
+//               <KeyboardArrowRight />
+//             ) : (
+//               <KeyboardArrowLeft />
+//             )}
+//             Back
+//           </Button>
+//         }
+//       />
+//     </Box>
+//   );
+// }
 
 export const cardNews = () => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
-
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
+    <>
+      <Swiper
+        slidesPerView={1}
+        autoHeight={true}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
         }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+        className="mySwiper"
       >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
-  );
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  )
 }
--- mimos_site/src/page/home.tsx
+++ mimos_site/src/page/home.tsx
@@ -2,29 +2,37 @@ import MenuBar from '../components/cpt_menu';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { cardNews } from '../components/cpt_cardNews';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 
 const Home = () => (
     // <div className="text-white bg-primary w-full overflow-hidden">
-    <div className="backGroudDiv">
-        <div>
+    <>
+        <header>
             <MenuBar/>
+        </header>
+        <div>
+            <div className="backGroudDiv">
+                <div>
+                    <Stack sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
+                        <Typography variant="h6" gutterBottom>
+                            Bem-vindo à página Mimos da Ana
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Escolha a categoria abaixo para ver exemplos dos nossos produtos e faça a sua encomenda.
+                        </Typography>
+                    </Stack>
+                </div>
+            </div>
+            {/* Ajustar o css desse swiper por que está espremido */}
+            <div className="news-card">
+                <Grid container spacing={2} justifyContent="center">
+                    {cardNews()}
+                </Grid>
+            </div>
         </div>
-        <div className="backGroudDiv">
-            <Stack sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-                <p>
-                    Bem vindo a pagina Mimos da Ana
-                </p>
-                <p>
-                    Escolha a categoria abaixo para ter exemplos dos nossos produtos.
-                    Faça a sua encomenda
-                </p>
-            </Stack>
-            <Stack sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                {cardNews()}
-            </Stack>
-        </div>
-    </div>
+    </>
 )
 
 export default Home
